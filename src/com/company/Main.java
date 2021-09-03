d71ffdd16a94561abd8adc51540055b82061b7ac
adelwww <wsnezka@gmail.com>
2021-09-03 20:04:34 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //homework1
+        String s1 = "Аделя ";
+        String s2 = "Сатыбалдиева";
+        System.out.println("Меня зовут " + s1 + s2);
+
+        // задание на смекалку
+
+        System.out.println("      *          ******     * * * *        *         * * *");
+        System.out.println("     * *         *    *     *             * *        *   *");
+        System.out.println("    *   *        *    *     *            *   *       *   *");
+        System.out.println("   * * * *     * * * * * *  * * *       *     *      * * *");
+        System.out.println("  *       *    *         *  *          *       *        * *");
+        System.out.println(" *         *   *         *  *         *         *      *   *");
+        System.out.println("*           *  *         *  * * * *  *           *    *     *");
+
+    }
+}
